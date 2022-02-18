be04dd232b1b568464c597ed35fe93353e19ecfc
wodyjowski <mkisiel58@gmail.com>
2022-02-18 10:38:10 +0100

    Initial commit

---
+++ React/Pages/Test/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace React.Pages.Test
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
